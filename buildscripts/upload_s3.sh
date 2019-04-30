5ec8a6a2a6941ff94c42dfeb68afc1fa6dcba81e
Harley Thorne <harleyjthorne@gmail.com>
2019-04-30 16:17:16 +0100

    Switch to blog.bethanywrites.co.uk

--- buildscripts/upload_s3.sh
+++ buildscripts/upload_s3.sh
@@ -2,4 +2,4 @@
 echo "Uploading to s3"
 export AWS_ACCESS_KEY_ID=$1
 export AWS_SECRET_ACCESS_KEY=$2
-aws s3 sync public/ s3://new.bethanywrites.co.uk --delete
+aws s3 sync public/ s3://blog.bethanywrites.co.uk --delete
